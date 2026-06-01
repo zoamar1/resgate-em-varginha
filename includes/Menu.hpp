3c7cfb137bfa27bb86d4040ba914b1902abbeb6a
zoamar1 <boreal.cpps@gmail.com>
2026-05-31 21:00:46 -0300

    fazendo menu

--- includes/Menu.hpp
+++ includes/Menu.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "Header.hpp"
 #include "entidades/Ente.hpp"
+#include <vector>
+#include <string>
 
 class Jogo;
 
@@ -8,9 +10,17 @@ class Menu : public Ente
 {
     private:
         Jogo* pJogo;
+        
+        sf::Font fonte_padrao;
+        sf::Text titulo;
+        std::vector<sf::Text> opcoes_menu;
+
+    private:
+        void formataTexto(sf::Text& texto, const std::string& frase, int tamanho, float posY);
+
     public:
         Menu();
         ~Menu();
-        void desenhaTexto();
         void executar();
+        void desenhaTexto();
 };
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -5,23 +5,49 @@ Menu::Menu() : Ente()
     pFig->setSize({LARGURA, ALTURA});
     pFig->setOrigin({0, 0});
     aplicarTextura(Gerenciadores::Menu);
+
+    if (!fonte_padrao.loadFromFile("assets/fonts/AlfaSlabOne-Regular.ttf")) {
+        std::cout << "fonte deu erro" << std::endl;
+    }
+
+    formataTexto(titulo, "Resgate em Varginha", 60, 200.0f);
+    titulo.setFillColor(sf::Color::Green);
+
+    sf::Text opcao_jogar;
+    formataTexto(opcao_jogar, "Jogar", 40, 400.0f);
+    
+    sf::Text opcao_sair;
+    formataTexto(opcao_sair, "Sair", 40, 500.0f);
+
+    opcoes_menu.push_back(opcao_jogar);
+    opcoes_menu.push_back(opcao_sair);
 };
 
 Menu::~Menu()
 {
 }
 
+void Menu::formataTexto(sf::Text& texto, const std::string& frase, int tamanho, float posY)
+{
+    texto.setFont(fonte_padrao);
+    texto.setString(frase);
+    texto.setCharacterSize(tamanho);
+    texto.setFillColor(sf::Color::White);
+
+    sf::FloatRect limites = texto.getLocalBounds();
+    texto.setOrigin(limites.left + limites.width / 2.0f,
+                    limites.top  + limites.height / 2.0f);
+                     
+    texto.setPosition(LARGURA / 2.0f, posY);
+}
+
 void Menu::desenhaTexto()
 {
-    sf::Font fonte_titulo;
-    fonte_titulo.loadFromFile("assets/fonts/AlfaSlabOne-Regular.ttf");
-    sf::Text titulo("Resgate em Varginha", fonte_titulo, 60);
-    titulo.setFillColor(sf::Color::Green);
-    sf::FloatRect limitesTexto = titulo.getLocalBounds();
-    titulo.setOrigin(limitesTexto.left + limitesTexto.width / 2.0f,
-                     limitesTexto.top + limitesTexto.height / 2.0f);
-    titulo.setPosition(LARGURA / 2, 200);
     pGG->desenhaTexto(titulo);
+
+    for (int i = 0; i < opcoes_menu.size(); i++) {
+        pGG->desenhaTexto(opcoes_menu[i]);
+    }
 }
 
 void Menu::executar()
