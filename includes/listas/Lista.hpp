86bd8a6d1e642193b0e4fff895c4c7c20a5a50ee
Eduardo <eduardo.machado2505@gmail.com>
2026-06-07 22:06:19 -0300

    adicionando metodo remover

--- includes/listas/Lista.hpp
+++ includes/listas/Lista.hpp
@@ -1,104 +1,159 @@
 #pragma once
 #include "Header.hpp"
-namespace Listas {
+namespace Listas
+{
 
-    template<class TL>
-    class Lista {
+    template <class TL>
+    class Lista
+    {
     private:
-        template<class TE>
-        class Elemento {
+        template <class TE>
+        class Elemento
+        {
         private:
-            Elemento<TE>* pProx;
-            TE* pInfo;
+            Elemento<TE> *pProx;
+            TE *pInfo;
 
         public:
-            Elemento() {
-                pProx = NULL; 
+            Elemento()
+            {
+                pProx = NULL;
                 pInfo = NULL;
             }
-            
-            ~Elemento() {
+
+            ~Elemento()
+            {
             }
-            
-            void incluir(TE* p) {
+
+            void incluir(TE *p)
+            {
                 pInfo = p;
                 setProx(NULL);
             }
-            
-            void setProx(Elemento<TE>* pE) {
+
+            void setProx(Elemento<TE> *pE)
+            {
                 pProx = pE;
             }
-            
-            Elemento<TE>* getProximo() const { 
+
+            Elemento<TE> *getProximo() const
+            {
                 return pProx;
             }
-            
-            TE* getInfo() const {
+
+            TE *getInfo() const
+            {
                 return pInfo;
             }
         };
 
-        Elemento<TL>* pPrimeiro;
-        Elemento<TL>* pUltimo;
-        Elemento<TL>* pAtual;
+        Elemento<TL> *pPrimeiro;
+        Elemento<TL> *pUltimo;
+        Elemento<TL> *pAtual;
+
     public:
-        Lista() {
+        Lista()
+        {
             pPrimeiro = NULL;
             pUltimo = NULL;
             pAtual = NULL;
         }
-        
-        ~Lista() {
+
+        ~Lista()
+        {
             limpar();
         }
-        
-        void incluir(TL* p) {
-            if (!p) return;
-            
-            if (!pPrimeiro) {
+
+        void incluir(TL *p)
+        {
+            if (!p)
+                return;
+
+            if (!pPrimeiro)
+            {
                 pPrimeiro = new Elemento<TL>();
                 pPrimeiro->incluir(p);
                 pUltimo = pPrimeiro;
-            } else {
-                Elemento<TL>* aux = new Elemento<TL>(); 
+            }
+            else
+            {
+                Elemento<TL> *aux = new Elemento<TL>();
                 aux->incluir(p);
                 pUltimo->setProx(aux);
                 pUltimo = aux;
             }
         }
-        
-        void limpar() {
-            Elemento<TL>* aux = pPrimeiro; 
-            
-            while(aux) {
-                Elemento<TL>* prox = aux->getProximo();
+
+        void limpar()
+        {
+            Elemento<TL> *aux = pPrimeiro;
+
+            while (aux)
+            {
+                Elemento<TL> *prox = aux->getProximo();
                 delete aux;
                 aux = prox;
             }
-            
+
             pPrimeiro = NULL;
             pUltimo = NULL;
         }
 
-        TL* getPrimeiro() {
+        void remover(TL *p)
+        {
+            if (!p || !pPrimeiro)
+                return;
+
+            if (pPrimeiro->getInfo() == p)
+            {
+                Elemento<TL> *aux = pPrimeiro;
+                pPrimeiro = pPrimeiro->getProximo();
+                if (!pPrimeiro)
+                    pUltimo = NULL;
+                delete aux;
+                return;
+            }
+
+            Elemento<TL> *anterior = pPrimeiro;
+            Elemento<TL> *atual = pPrimeiro->getProximo();
+            while (atual)
+            {
+                if (atual->getInfo() == p)
+                {
+                    anterior->setProx(atual->getProximo());
+                    if (atual == pUltimo)
+                        pUltimo = anterior;
+                    delete atual;
+                    return;
+                }
+                anterior = atual;
+                atual = atual->getProximo();
+            }
+        }
+
+        TL *getPrimeiro()
+        {
             pAtual = pPrimeiro;
-            if (pAtual) {
+            if (pAtual)
+            {
                 return pAtual->getInfo();
             }
             return NULL;
         }
 
-        TL* getProximo() {
-            if (pAtual) {
-                pAtual = pAtual->getProximo(); 
+        TL *getProximo()
+        {
+            if (pAtual)
+            {
+                pAtual = pAtual->getProximo();
             }
-            
-            if (pAtual) {
+
+            if (pAtual)
+            {
                 return pAtual->getInfo();
             }
             return NULL;
         }
-
     };
 
 }
